854aec4cbe64d328bd34464190f5f1dca4d6434f
callmemiya <56102704+callmemiya@users.noreply.github.com>
2020-02-22 09:47:43 +0300

    Initial Commit

---
+++ lab2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab2
+//
+//  Created by Мария Забродина on 22/02/2020.
+//  Copyright © 2020 Мария Забродина. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
